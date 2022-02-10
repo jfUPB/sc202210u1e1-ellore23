151f32262d82cd5c75b98d374b142403119295fc
Lorenzo_FlorezV <florezvalencialorenzo@gmail.com>
2022-02-09 23:44:33 -0500

    Detecta tecla v1

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,17 @@
 // Lorenzo Florez Valencia
 // 000418868
 // lorenzo.florez@upb.edu.co
+
+//Empiezo programa
 (START)
-    @START
+    @START // Detectar tecla F
+    @24576
+    D=M  // Guardo lo que hay en A (Solo F)
+    D-24576
+    0;JEQ  //
+    @1
+    0;JGT 
+    
+    
+    
     0;JMP
