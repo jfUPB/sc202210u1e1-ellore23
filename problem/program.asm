50393591a4a13713f646317150767806918322d9
Lorenzo_FlorezV <florezvalencialorenzo@gmail.com>
2022-02-11 10:45:04 -0500

    Detecta F y llena pantalla

--- problem/program.asm
+++ problem/program.asm
@@ -2,18 +2,43 @@
 // 000418868
 // lorenzo.florez@upb.edu.co
 
-//Empiezo programa
 (START)
-    @START // Detectar tecla F
-    @24576
-    D=M  // Guardo lo que hay en A Solo F
-    @70
-    D=D-A
-    @10
-    D;JEQ  // Salta si se presiona F
-    @0
-    D=A
-    D;JEQ  
-    
-    
-    
+  @16384 //Primer punto pixel
+  D=A 
+  @1 //Lugar donde guardo primera posicion
+  M=D
+
+  //Loop 1
+  @1 // Lugar donde guardo primera posicion
+  D=M // Preparo
+  @2 //Inicia la pantalla
+  M=D
+
+  //Detecto el input F
+  @24576 //input
+  D=M
+  @70
+  D=D-A
+  @16
+  D;JEQ
+  @8
+  D;JLT
+  @8
+  D;JGT
+
+  //Loop 2 Mientras que siga en pantalla y F //12
+  @2
+  D=M
+  @24575
+  D=D-A
+  @4
+  D;JGT
+
+  //Coloco valores
+  @2
+  A=M
+  M=1
+  @2
+  M=M+1
+  @18
+  0;JMP
