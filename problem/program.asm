9c4116cda5cb6e88bd906641b32fb4b84ba846ab
Lorenzo_FlorezV <florezvalencialorenzo@gmail.com>
2022-02-13 01:48:14 -0500

    Version final

--- problem/program.asm
+++ problem/program.asm
@@ -21,41 +21,42 @@
   D=D-A
   @16
   D;JEQ
-  @8
+  @4
   D;JLT
-  @8
+  @4
   D;JGT
 
   //Loop 2 Mientras que siga en pantalla y F //12
   @2
   D=M
-  @24575
+  @24576
   D=D-A
-  @4
-  D;JGT
-  @33 // Salto a det
-  D;JEQ
+  @31
+  D;JGE
+
 
   //Coloco valores
   @2
   A=M
-  M=1
+  M=-1
   @2
   M=M+1
   @18
   0;JMP
+  
+  // Reinicio primera posicion
+  @1 // Lugar donde guardo primera posicion
+  D=M // Preparo
+  @2 //Inicia la pantalla
+  M=D
 
   // Loop 3 Detecto input C // 33
   @24576
   D=M
   @67
   D=D-A
-  @42
-  D;JEQ //Salta aqui si es C
-  @33
-  D;JGT
-  D;JLT //Salta aqui si no es C
-
+  @35
+  D;JNE //Salta aqui si no es C
   
 
 //Loop 4 Mientras que siga en pantalla 
@@ -63,12 +64,8 @@
   D=M 
   @24575
   D=D-A
-  @48 
+  @4
   D;JGT
-  @5
-  D;JEQ
-
-  //Hasta aca va bien
   
   //Coloco valores
   @2
@@ -76,5 +73,5 @@
   M=0
   @2
   M=M+1
-  @42
+  @41
   0;JMP
